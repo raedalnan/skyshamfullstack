9be8b04dbcf499f9a2d832b81a0f58045989c923
Frontend Savvy <lukmansk602@gmail.com>
2025-05-05 21:38:51 +0530

    change config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      new URL('https://randomuser.me/api/portraits/**'), 
-      new URL('https://upload.wikimedia.org/**')],
+    domains: ["randomuser.me", "upload.wikimedia.org"],
   },
 };
 
